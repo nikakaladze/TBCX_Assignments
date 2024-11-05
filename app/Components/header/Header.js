833e8d18b20732f4b57547c31e844bf78b430ff6
nikakaladze <nikakaladze89@gmail.com>
2024-11-05 13:04:37 +0400

    feat:Working Tailwind in Profile Page

--- app/Components/header/Header.js
+++ app/Components/header/Header.js
@@ -34,12 +34,9 @@ export default function Header() {
                 Products
               </span>
             </Link>
-            <a
-              href="/api/auth/login"
-              className="text-[20px] mr-[30px] hover:text-white hover:bg-gradient-to-r from-blue-500 to-green-500 active:bg-gradient-to-r from-blue-500 to-green-500 p-[10px] rounded-[10px]"
-            >
-              Login
-            </a>
+
+            <a href="/api/auth/login" className="text-[20px] mr-[30px] hover:text-white hover:bg-gradient-to-r from-blue-500 to-green-500 active:bg-gradient-to-r from-blue-500 to-green-500 p-[10px] rounded-[10px]">Login</a>
+            <a href="/api/auth/logout" className="text-[20px] mr-[30px] hover:text-white hover:bg-gradient-to-r from-blue-500 to-green-500 active:bg-gradient-to-r from-blue-500 to-green-500 p-[10px] rounded-[10px]">Logout</a>
             
           </div>
         </div>
--- app/page.js
+++ app/page.js
@@ -6,7 +6,6 @@ import { getSession } from "@auth0/nextjs-auth0";
 export default async function Home() {
   const session = await getSession();
   const user = session?.user;
-  console.log(user);
 
   return (
     <>
