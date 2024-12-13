42449a7e3acd81e3dfda8bea72dd4291f10fe27e
nikakaladze <nikakaladze89@gmail.com>
2024-12-13 16:47:56 +0400

    create interface to images in img.js and rewrite to img.ts

--- app/Components/MainPage/productCardImages/img.ts
+++ app/Components/MainPage/productCardImages/img.ts
@@ -1,4 +1,8 @@
-export const images = [
+export interface Image {
+  imgUrl: string;
+}
+
+export const images: Image[] = [
   {
     imgUrl:
       "https://lsco.scene7.com/is/image/lsco/857440000-dynamic1-pdp-lse?fmt=jpeg&qlt=70&resMode=sharp2&fit=crop,1&op_usm=0.6,0.6,8&wid=2000&hei=1840",
@@ -6,16 +10,13 @@ export const images = [
   {
     imgUrl: "https://pajaksport.pl/1788-medium_default/the-coat-d.jpg",
   },
-
   {
     imgUrl: "https://m.media-amazon.com/images/I/71Pw0DynB+L._AC_UY1000_.jpg",
   },
-
   {
     imgUrl:
       "https://assets.vogue.com/photos/650c802b5ef1aa00e66620a6/3:4/w_748%2Cc_limit/slide_2.jpg",
   },
-
   {
     imgUrl:
       "https://media.boohoo.com/i/boohoo/fzz69379_khaki_xl?w=900&qlt=default&fmt.jp2.qlt=70&fmt=auto&sm=fit",
@@ -24,12 +25,10 @@ export const images = [
     imgUrl:
       "https://www.garmentprinting.com/wp-content/uploads/2022/02/jumper.jpg",
   },
-
   {
     imgUrl:
       "https://img01.ztat.net/article/spp-media-p1/01d4d652dbd3432e8261b947cb62794d/530451d8c3fc4703baf4a3a0594d383b.jpg",
   },
-
   {
     imgUrl:
       "https://store.moma.org/cdn/shop/files/300010_b_c5f441b6-0572-482a-89b1-4c89e2ddcd42.jpg?v=1705510937",
