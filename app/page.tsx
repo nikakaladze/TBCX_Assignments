21fc90aad1345bc28ba5c4c744d544dfc1c5a868
nikakaladze <nikakaladze89@gmail.com>
2024-12-13 17:11:22 +0400

    rewrite Main page.js to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import { ProductCardContainer } from "./Components/MainPage/ProductCardContainer/ProductCardContainer";
+import React from "react";
+import ProductCardContainer from "./ProductCardContainer";
 
-
-
-export default async function Home() {
-  
-
+export default async function Home(): Promise<JSX.Element> {
   return (
     <>
-    
       <ProductCardContainer />
     </>
   );
