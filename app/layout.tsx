1db7a83a2accdb835a7fd8d96d6135102512529e
nikakaladze <nikakaladze89@gmail.com>
2024-12-13 17:10:02 +0400

    rewrite Main Layout.js to Layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,19 +3,26 @@ import Header from "./Components/header/Header";
 import Footer from "./Components/footer/Footer";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { ThemeProvider } from "next-themes";
+import { ReactNode } from "react";
 
-export default function RootLayout({ children }) {
+
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps): JSX.Element {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en" suppressHydrationWarning>
       <UserProvider>
         <body>
           <ThemeProvider attribute="class" defaultTheme="system">
-          <Header />
-          {children}
-          <Footer />
-      </ThemeProvider >
+            <Header />
+            {children}
+            <Footer />
+          </ThemeProvider>
         </body>
       </UserProvider>
     </html>
   );
 }
+
