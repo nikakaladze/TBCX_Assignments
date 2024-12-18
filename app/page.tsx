be7e25232fb1b88622e6212a523f075e67d61a91
nikakaladze <nikakaladze89@gmail.com>
2024-12-18 15:46:23 +0400

    add Componets and Donate with-Checkout Folders and his contents

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,38 @@
+import Link from "next/link";
 import { ProductCardContainer } from "./Components/MainPage/ProductCardContainer/ProductCardContainer";
 import React from "react";
 
 export default async function Home(): Promise<JSX.Element> {
   return (
     <>
-      <ProductCardContainer />
+      <ul className="card-list">
+        <li>
+          <Link
+            href="/donate-with-embedded-checkout"
+            className="card checkout-style-background"
+          >
+            <h2 className="bottom">Donate with embedded Checkout</h2>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/donate-with-checkout"
+            className="card checkout-style-background"
+          >
+            <h2 className="bottom">Donate with hosted Checkout</h2>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/donate-with-elements"
+            className="card elements-style-background"
+          >
+            <h2 className="bottom">Donate with Elements</h2>
+          </Link>
+        </li>
+      </ul>
+
+      {/* <ProductCardContainer /> */}
     </>
   );
 }
